d45d2fa96e40945b7533c80a057a479757163e33
Mateusz Sołtys <mateusz.soltys@god.dev>
2024-11-17 21:56:36 +0100

    kolorki

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ window.addEventListener('DOMContentLoaded', event => {
             return;
         }
 
-        if (window.scrollY === 0) {
+        if (window.scrollY < 5) {
             navbarCollapsible.classList.remove('navbar-shrink');
             // Show light logo, hide dark logo
             logoLight.classList.remove('d-none');
